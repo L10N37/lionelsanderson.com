5f50e73a4fbe8b77211348ea4c614c9de366d8c4
Lionel Sanderson <lfsanderson@hotmail.comgit config --global user.name Lionel>
2023-11-01 17:16:23 +1100

    update

--- js/splide.js
+++ js/splide.js
@@ -1,24 +1,21 @@
-const hobbyistSplide = new Splide( '#hobbyistCarousel', {
-    type   : 'loop',
-    drag   : 'free',
-    focus  : 'center',
+const hobbyistSplide = new Splide('#hobbyistCarousel', {
+    type: 'loop',
+    drag: 'free',
+    focus: 'center',
     perPage: 1,
-    autoScroll: {
-        speed: 1,
-      },
-});
-
-const fullStackSplide = new Splide( '#fullStackCarousel', {
-    type   : 'loop',
-    drag   : 'free',
-    focus  : 'center',
-    perPage: 1,
-    autoScroll: {
-        speed: 1,
-      },
-    
-});
-
+    autoplay: true, // Enable auto-scrolling
+    interval: 1500, // Set the time interval (in milliseconds)
+  });
   
-hobbyistSplide.mount();
-fullStackSplide.mount();
+  const fullStackSplide = new Splide('#fullStackCarousel', {
+    type: 'loop',
+    drag: 'free',
+    focus: 'center',
+    perPage: 1,
+    autoplay: true, // Enable auto-scrolling
+    interval: 1500, // Set the time interval (in milliseconds)
+  });
+  
+  hobbyistSplide.mount();
+  fullStackSplide.mount();
+  
